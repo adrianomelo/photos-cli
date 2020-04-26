3f13ecadd936d10f4bc7dc5066080214e7a3612d
Adriano Melo <github@adrianomelo.com>
2020-04-26 11:33:15 +0200

    Add cmdargs dependency & delete labal file

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# OPTIONS_GHC -fno-cse #-}
+
 module Main where
 
+import System.Console.CmdArgs
 import Lib
 
-main :: IO ()
-main = someFunc
+data Photos
+  = Upload {originDir :: String}
+  | Rename {sourceDir :: String}
+  deriving (Show, Data, Typeable)
+
+upload = Upload{originDir = def}
+
+rename = Rename{sourceDir = def}
+  &= auto
+
+commands = modes [rename, upload]
+  &= help "Organize photos"
+
+main = print =<< cmdArgs commands
+
